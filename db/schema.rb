39ed744f366f1bc494eb3661af0eeb4e53b9405a
セミ141 <whompsfortress141@gmail.com>
2025-11-08 16:51:18 +0900

    Devise用のencrypted_passwordカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_08_072415) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_08_074836) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_08_072415) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "encrypted_password", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
