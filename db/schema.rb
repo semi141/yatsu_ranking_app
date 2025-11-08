4c1781534f738b93b4e4c3404ce1ec71068cfcbb
セミ141 <whompsfortress141@gmail.com>
2025-11-08 16:36:15 +0900

    Deviseでユーザー登録・ログイン・編集・削除機能を実装 - UserモデルにDeviseを統合 - 必要なマイグレーション作成 & dbに反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_08_070918) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_08_072415) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
@@ -27,6 +27,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_08_070918) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "videos", force: :cascade do |t|
