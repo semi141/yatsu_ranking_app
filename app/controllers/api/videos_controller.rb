f69082271619e33db87927be5f433f0552014b91
セミ141 <whompsfortress141@gmail.com>
2025-11-16 01:33:48 +0900

    ルーティングに合わせて api/video_watched_controller を api/videos_controller にリネーム

---
+++ app/controllers/api/videos_controller.rb
@@ -0,0 +1,21 @@
+class Api::VideosController < ApplicationController
+  before_action :authenticate_user!
+
+  def watched
+    video_id = params[:video_id]
+    return head :bad_request unless video_id
+
+    video = Video.find_or_create_by!(youtube_id: video_id) do |v|
+      info = YoutubeService.get_video_info(video_id)  # new じゃない！
+      v.title = info[:title]
+      v.thumbnail = info[:thumbnail]
+      v.published_at = info[:published_at]
+    end
+
+    Post.find_or_create_by!(user: current_user, video: video) do |p|
+      p.content = "YouTubeで視聴！自動登録"
+    end
+
+    head :ok
+  end
+end
