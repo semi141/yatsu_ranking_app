a8a38dc71b3caa1cfb592c0c7e406ea3439e4e3b
セミ141 <whompsfortress141@gmail.com>
2025-11-16 12:51:37 +0900

    サーバー起動安定バージョン: APIルーティング前

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,4 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
