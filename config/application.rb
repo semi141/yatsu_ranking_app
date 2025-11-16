a85d47d0683da82c54e53b1f909f9b1a0065835e
セミ141 <whompsfortress141@gmail.com>
2025-11-16 13:22:04 +0900

    Cookieがブロックされる問題に対応: SameSite=None設定とsession_store調整

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,14 @@ Bundler.require(*Rails.groups)
 
 module YatsuRankingApp
   class Application < Rails::Application
+    config.action_dispatch.cookies_same_site_protection = :none
+
     config.load_defaults 7.2
 
     config.autoload_paths << Rails.root.join('app', 'services')
     
     config.autoload_lib(ignore: %w[assets tasks])
+
+    config.session_store :cookie_store, key: '_yatsu_ranking_app_session', secure: Rails.env.production?
   end
 end
