4f0723100d3529b5b5c58b15ce04116a5a4b5e1a
セミ141 <whompsfortress141@gmail.com>
2025-11-16 15:41:59 +0900

    開発環境でCookieを確実に送るため domain: :all を追加

--- config/application.rb
+++ config/application.rb
@@ -8,17 +8,16 @@ module YatsuRankingApp
   class Application < Rails::Application
     config.load_defaults 7.2
 
-    # Cookie設定（最優先で上書き）
+    # Cookie設定（最強版！）
     config.action_dispatch.cookies_same_site_protection = :none
     config.force_ssl = false
 
-    # セッション設定（secure: false 強制）
     config.session_store :cookie_store, 
                          key: '_yatsu_ranking_app_session', 
                          secure: false, 
-                         same_site: :none
+                         same_site: :none,
+                         domain: :all
 
-    # サービスパス
     config.autoload_paths << Rails.root.join('app', 'services')
     config.autoload_lib(ignore: %w[assets tasks])
   end
