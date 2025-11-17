1e8d93ceb566706e166245bd016a1bfdb193b6c4
セミ141 <whompsfortress141@gmail.com>
2025-11-17 12:24:05 +0900

    Googleログイン不具合の原因だったSameSite設定を削除し、セッション設定を正常化

--- config/application.rb
+++ config/application.rb
@@ -9,15 +9,15 @@ module YatsuRankingApp
     config.load_defaults 7.2
 
     # Cookie設定（最強版！）
-    config.action_dispatch.cookies_same_site_protection = :none
+    #config.action_dispatch.cookies_same_site_protection = :none
     config.force_ssl = false
 
-    config.session_store :cookie_store, 
-                         key: '_yatsu_ranking_app_session', 
-                         secure: false, 
-                         same_site: :none,
-                         domain: :all
-
+    #config.session_store :cookie_store, 
+                         #key: '_yatsu_ranking_app_session', 
+                         #secure: false, 
+                         #same_site: :none,
+                         #domain: :all
+    config.session_store :cookie_store, key: '_yatsu_ranking_app_session'
     config.autoload_paths << Rails.root.join('app', 'services')
     config.autoload_lib(ignore: %w[assets tasks])
   end
