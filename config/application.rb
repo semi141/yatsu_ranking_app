6e6c57e1edb4a37416c1083e89d5f30085ddfe2f
セミ141 <whompsfortress141@gmail.com>
2025-11-17 12:37:29 +0900

    UserにAPIトークン(api_token)を追加するマイグレーションを作成・適用

--- config/application.rb
+++ config/application.rb
@@ -7,16 +7,7 @@ Bundler.require(*Rails.groups)
 module YatsuRankingApp
   class Application < Rails::Application
     config.load_defaults 7.2
-
-    # Cookie設定（最強版！）
-    #config.action_dispatch.cookies_same_site_protection = :none
     config.force_ssl = false
-
-    #config.session_store :cookie_store, 
-                         #key: '_yatsu_ranking_app_session', 
-                         #secure: false, 
-                         #same_site: :none,
-                         #domain: :all
     config.session_store :cookie_store, key: '_yatsu_ranking_app_session'
     config.autoload_paths << Rails.root.join('app', 'services')
     config.autoload_lib(ignore: %w[assets tasks])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_16_070820) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_17_033323) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -40,7 +40,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_16_070820) do
     t.string "youtube_refresh_token"
     t.datetime "youtube_expires_at"
     t.string "api_key"
+    t.string "api_token"
     t.index ["api_key"], name: "index_users_on_api_key", unique: true
+    t.index ["api_token"], name: "index_users_on_api_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid"], name: "index_users_on_uid"
