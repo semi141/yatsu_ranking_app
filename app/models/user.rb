866066f65d03ed32e1b7fe7b88af0c944020bdff
セミ141 <whompsfortress141@gmail.com>
2025-11-17 12:58:35 +0900

    UserモデルにAPIトークン生成処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,42 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable,
-        :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
+  # 拡張機能用APIトークン
+  before_create :generate_api_token
+
+  has_many :posts, dependent: :destroy
+  has_many :videos, through: :posts
+
+  # Google OAuthでログイン/登録してきたときの処理
   def self.from_omniauth(auth)
     user = where(provider: auth.provider, uid: auth.uid).first_or_initialize
-    user.email = auth.info.email
-    user.name = auth.info.name
-    user.password ||= Devise.friendly_token[0, 20]  # すでにパスワードがあれば上書きしない
-    user.access_token = auth.credentials.token
-    user.refresh_token = auth.credentials.refresh_token if auth.credentials.refresh_token.present?
+    user.email          ||= auth.info.email
+    user.name           ||= auth.info.name
+    user.password       ||= Devise.friendly_token[0, 20]
+    user.access_token   = auth.credentials.token
+    user.refresh_token  = auth.credentials.refresh_token if auth.credentials.refresh_token.present?
+
+    # Googleログインでもapi_tokenが空なら生成する
+    user.generate_api_token if user.api_token.blank?
+    
     user.save!
     user
+  end
+
+  # トークン再生成用（ボタン押したとき用）
+  def regenerate_api_token
+    self.api_token = SecureRandom.hex(20)
+    save!
+  end
+
+  private
+
+  def generate_api_token
+    loop do
+      self.api_token = SecureRandom.hex(20)
+      break unless User.exists?(api_token: self.api_token)
+    end
   end
 end
