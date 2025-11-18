163a7a6a2b41c9fc2fc8ffb9a3c1875d2d87c213
セミ141 <whompsfortress141@gmail.com>
2025-11-18 18:59:35 +0900

    ランキング+視聴回数表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
-  def index
+  def ranking
+    if user_signed_in?
+      @my_rankings = current_user.videos
+                        .select('videos.*, COUNT(posts.id) AS view_count')
+                        .joins(:posts)
+                        .group('videos.id')
+                        .order('view_count DESC')
+    end
+
+    @all_rankings = Video
+                     .select('videos.*, COUNT(posts.id) AS total_views')
+                     .joins(:posts)
+                     .group('videos.id')
+                     .order('total_views DESC')
   end
 end
