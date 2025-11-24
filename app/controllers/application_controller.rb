3cb93e120f0b85fbe4e15e525f3ec860a7c80fe9
セミ141 <whompsfortress141@gmail.com>
2025-11-24 23:50:28 +0900

    feat: サイト内ログインフォームへの導線を削除し、全てのログインをGoogle認証に直結させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   protect_from_forgery with: :exception
+
+  # Deviseのデフォルトのログイン画面パス（/users/sign_in）をオーバーライドし、
+  # 認証が必要になったら、直接Google認証のパスにリダイレクトさせるメソッド
+  def new_session_path(scope)
+    # :user スコープで :google_oauth2 プロバイダへの認証開始パスを返す
+    return omniauth_authorize_path(:user, :google_oauth2)
+  end
 end
