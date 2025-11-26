d7e131981780fdc5407493c073a85a92697fe101
セミ141 <whompsfortress141@gmail.com>
2025-11-26 14:21:29 +0900

    fix: 定数GUEST_USER_EMAILの定義場所を修正し、Heroku起動エラーを解消

--- app/controllers/users/guest_logins_controller.rb
+++ app/controllers/users/guest_logins_controller.rb
@@ -1,19 +1,19 @@
+GUEST_USER_EMAIL = "guest@example.com"
+
 class Users::GuestLoginsController < ApplicationController
-  # ログイン状態でも実行できるようにする
+  # ログイン状態でも実行できるように
   before_action :authenticate_user!, except: [:create]
 
   # ゲストログイン処理
   def create
-    # db/seeds.rb で定義したゲストユーザーのメールアドレス
-    GUEST_USER_EMAIL = "guest@example.com"
-
-    # 1. ゲストユーザーを見つける
+    
+    # ゲストユーザーを見つける
     user = User.find_by!(email: GUEST_USER_EMAIL)
     
-    # 2. ユーザーをログインさせる (Deviseのヘルパーメソッド)
+    # ユーザーをログインさせる (Deviseのヘルパーメソッド)
     sign_in user 
     
-    # 3. ログイン後のページにリダイレクト
+    # ログイン後のページにリダイレクト
     redirect_to root_path, notice: "ゲストユーザーとしてログインしました！"
   end
 end
